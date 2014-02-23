44d2f1845e9ec8f8272ddeff6b348844a6bb177f
sugarev <boris.sugar@gmail.com>
2014-02-23 13:50:37 +0200

    .init

---
+++ OOPFundamentalsPartI/AnimalProject/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace AnimalProject
+{
+    using System;
+    public abstract class Animal : ICharacteristics, ISound
+    {
+        public Animal(string name, double age, Sex sex)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Sex = sex;
+        }
+
+        public string Name { get; private set; }
+
+        public double Age { get; private set; }
+
+        public Sex Sex { get; private set; }
+
+        public abstract void Sound();
+    }
+}
